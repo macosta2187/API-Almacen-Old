efd5de4c71389ad4775846b1a640cf0baa00b898
Michael <michael.acostaperez1987@gmail.com>
2023-08-19 23:33:59 -0300

    Se crea proyecto

--- app/Http/Middleware/Autenticacion.php
+++ app/Http/Middleware/Autenticacion.php
@@ -10,13 +10,33 @@ class Autenticacion
 {
 
 
+/*
  public function handle(Request $request, Closure $next)
  {
    $tokenHeader = [ "Authorization" => $request -> header("Authorization")];
    $response = Http::withHeaders($tokenHeader) -> get("http://127.0.0.1:8001/api/validate");
+
+   
    if($response -> successful() && $response -> body() != "")
          return $next($request);
   return response(["message" => "No autorizado"], 403);
   
  }
+ */
+ 
+
+ public function handle(Request $request, Closure $next)
+    {
+        $tokenHeader = [ "Authorization" => $request -> header("Authorization")];
+
+        $response = Http::withHeaders($tokenHeader) -> get("http://127.0.0.1:8001/api/validate");
+        if($response -> successful())
+            return $next($request);
+        return response(["message" => "No autorizado"], 403);
+
+        
+    }
+
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,3 @@
 <?php
 
-use App\Http\Controllers\AlmacenController;
-use App\Http\Controllers\LoteController;
-use App\Http\Controllers\ProductoController;
-use App\Http\Middleware\Autenticacion;
-use Illuminate\Support\Facades\Route;
 
-Route::post('/Almacen', [AlmacenController::class, 'Insertar'])->middleware(Autenticacion::class);
-Route::get("/Almacen", [AlmacenController::class, "Listar"])->middleware(Autenticacion::class);
-Route::delete('/Almacen/{id}', [AlmacenController::class, 'Eliminar'])->middleware(Autenticacion::class);
-Route::post('/Almacen/{id}', [AlmacenController::class, 'Actualizar'])->middleware(Autenticacion::class);
-
-Route::post('/Producto', [ProductoController::class, 'Insertar'])->middleware(Autenticacion::class);
-Route::get("/Producto", [ProductoController::class, "Listar"])->middleware(Autenticacion::class);
-Route::delete('/Producto/{id}', [ProductoController::class, 'Eliminar'])->middleware(Autenticacion::class);
-Route::post('/Producto/{id}', [ProductoController::class, 'Actualizar'])->middleware(Autenticacion::class);
-
-Route::post('/Lote', [LoteController::class, 'Insertar'])->middleware(Autenticacion::class);
-Route::get("/Lote", [LoteController::class, "Listar"])->middleware(Autenticacion::class);
-Route::delete('/Lote/{id}', [LoteController::class, 'Eliminar'])->middleware(Autenticacion::class);
-Route::post('/Lote/{id}', [LoteController::class, 'Actualizar'])->middleware(Autenticacion::class);
